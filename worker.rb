1c218d1c2c7989cdb5988456b1f97d22c4cccea5
Matthew Galloway <mgalloway@silverbottle.net>
2017-01-20 19:23:59 -0600

    Keep score

--- worker.rb
+++ worker.rb
@@ -10,77 +10,84 @@ require 'text'
 
 SlackRubyBot::Client.logger.level = Logger::WARN
 
-class PongBot < SlackRubyBot::Bot
-  command 'ping' do |client, data, match|
-    client.say(text: 'pong', channel: data.channel)
-    EM.defer do
-      sleep 10
-      client.say(text: 'later', channel: data.channel)
-    end
-  end
-
+class JeopartyBot < SlackRubyBot::Bot
   match /^next answer/ do |client, data, match|
-    current_key = "#{data.channel}:current:answer"
+    current_key = "#{data.channel}:current"
 
     # Only post a question if none is in progress
-    if !$redis.exists(current_key)
+    unless $redis.exists(current_key)
 
       question = get_question
-      airdate = Time.iso8601(question["airdate"]).to_i
+      air_date = Time.iso8601(question['airdate']).to_i
 
-      client.web_client.chat_postMessage(
+      puts client.web_client.chat_postMessage(
           channel: data.channel,
           as_user: true,
           attachments: [
             {
-              fallback: "#{question["category"]["title"]} ($ #{question["value"]}): #{question["question"]}",
-              title: "#{question["category"]["title"]} for $#{question["value"]}",
-              text: "#{question["question"]}",
-              ts: "#{airdate}"
+              fallback: "#{question['category']['title']} for $#{question['value']}: `#{question['question']}`",
+              title: "#{question['category']['title']} for $#{question['value']}",
+              text: "#{question['question']}",
+              ts: "#{air_date}"
             }
           ]
       )
 
-      $redis.setex(current_key, 15, question["answer"])
+      $redis.setex(current_key, 15, question.to_json)
 
       EM.defer do
         sleep 10
         if $redis.exists(current_key)
           $redis.del(current_key)
-          client.say(text: "Time is up! The answer was \n> #{question["answer"]}", channel: data.channel)
+          client.say(text: "Time is up! The answer was \n> #{question['answer']}", channel: data.channel)
         end
       end
     end
   end
 
   match /^(what|whats|where|wheres|who|whos) /i do |client, data, match|
-
-    current_key = "#{data.channel}:current:answer"
+    current_key = "#{data.channel}:current"
     answer = $redis.get(current_key)
-    if !answer.nil?
-      if is_correct?(answer, data.text)
+
+    unless answer.nil?
+      answer = JSON.parse(answer)
+      if is_correct?(answer['answer'], data.text)
         $redis.del(current_key)
-        client.say(text: "You did it!", channel: data.channel)
+        score = update_score(data.channel, data.user, answer['value'])
+        client.say(text: "That is the correct answer <@#{data.user}>! Your score is now #{score}", channel: data.channel)
       else
-        client.say(text: "Nope :(", channel: data.channel)
+        score = update_score(data.channel, data.user, answer['value'] * -1)
+        client.say(text: "Sorry <@#{data.user}>, that is incorrect. Your score is now #{score}", channel: data.channel)
       end
+    end
+  end
+
+  match /^show score/ do |client, data, match|
+    score = get_score(data.channel, data.user)
+    client.say(text: "<@#{data.user}>, your score is #{score}", channel: data.channel)
+  end
+
+  match /^new game/ do |client, data, match|
+    game_key = "#{data.channel}:game"
+    if game_key.nil?
+      client.say(text:'starting a new game goes here', channel: data.channel)
     end
   end
 end
 
 def is_correct?(correct, response) 
-  correct = correct.gsub(/[^\w\s]/i, "")
-            .gsub(/^(the|a|an) /i, "")
+  correct = correct.gsub(/[^\w\s]/i, '')
+            .gsub(/^(the|a|an) /i, '')
             .strip
             .downcase
 
   response = response
-           .gsub(/\s+(&nbsp;|&)\s+/i, " and ")
-           .gsub(/[^\w\s]/i, "")
-           .gsub(/^(what|whats|where|wheres|who|whos) /i, "")
-           .gsub(/^(is|are|was|were) /, "")
-           .gsub(/^(the|a|an) /i, "")
-           .gsub(/\?+$/, "")
+           .gsub(/\s+(&nbsp;|&)\s+/i, ' and ')
+           .gsub(/[^\w\s]/i, '')
+           .gsub(/^(what|whats|where|wheres|who|whos) /i, '')
+           .gsub(/^(is|are|was|were) /, '')
+           .gsub(/^(the|a|an) /i, '')
+           .gsub(/\?+$/, '')
            .strip
            .downcase
 
@@ -92,17 +99,40 @@ def is_correct?(correct, response)
 end
 
 def get_question
-  uri = "http://jservice.io/api/random?count=1"
+  uri = 'http://jservice.io/api/random?count=1'
   request = HTTParty.get(uri)
   puts "[LOG] #{request.body}"
   response = JSON.parse(request.body).first
-  question = response["question"]
-  if question.nil? || question.strip == "" 
+  question = response['question']
+  if question.nil? || question.strip == ''
     response = get_question
   end
-  response["value"] = 200 if response["value"].nil?
-  response["answer"] = Sanitize.fragment(response["answer"].gsub(/\s+(&nbsp;|&)\s+/i, " and "))
+  response['value'] = 200 if response['value'].nil?
+  response['answer'] = Sanitize.fragment(response['answer'].gsub(/\s+(&nbsp;|&)\s+/i, ' and '))
   response
+end
+
+def get_score(channel, user)
+  key = "score:#{channel}:#{user}"
+  current_score = $redis.get(key)
+  if current_score.nil?
+    0
+  else
+    current_score.to_i
+  end
+end
+
+def update_score(channel, user, score = 0)
+  key = "score:#{channel}:#{user}"
+  current_score = $redis.get(key)
+  if current_score.nil?
+    $redis.set(key, score)
+    score
+  else
+    current_score = current_score.to_i + score
+    $redis.set(key, current_score)
+    current_score
+  end
 end
 
 EM.run do
@@ -115,9 +145,6 @@ EM.run do
   $stdout.sync = true
   
   # Set up redis
-  uri = URI.parse(ENV["REDIS_URL"])
+  uri = URI.parse(ENV['REDIS_URL'])
   $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-
 end
-	
-#PongBot.run
